f2c1619c848440ef33bdeb86f1f34169e01e6193
John Hewitt <jhnh198@gmail.com>
2024-04-29 21:28:30 -0500

    working on note builder controls

--- src/components/NoteBuilder.tsx
+++ src/components/NoteBuilder.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { CheckboxOptions } from './CheckboxOptions'
+import { Form } from 'react-bootstrap'
 
 export default function NoteBuilder() {
   return (
-    <div>NoteBuilder</div>
+    <>
+      <Form>
+      <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={15}
+          />
+      <CheckboxOptions />
+      </Form.Group>
+      </Form>
+    </>
+
   )
 }
